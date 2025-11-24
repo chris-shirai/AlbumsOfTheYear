62185103345b86c8ac6d789c29c10a6416c0e854
chris-shirai <cks@vt.edu>
2025-11-23 20:09:55 -0500

    Initial Commit

---
+++ AlbumsOfTheYear/AlbumsOfTheYearApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumsOfTheYearApp.swift
+//  AlbumsOfTheYear
+//
+//  Created by Chris Shirai on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct AlbumsOfTheYearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

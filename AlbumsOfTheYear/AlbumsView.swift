8c2b69a697f929005f8da3e43045b95dd1f9e59a
chris-shirai <cks@vt.edu>
2025-12-04 23:20:42 -0500

    Tab view with auth request

---
+++ AlbumsOfTheYear/AlbumsView.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumsView.swift
+//  AlbumsOfTheYear
+//
+//  Created by Chris Shirai on 12/4/25.
+//
+
+import SwiftUI
+
+struct AlbumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumsView()
+}
---
+++ AlbumsOfTheYear/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  AlbumsOfTheYear
+//
+//  Created by Chris Shirai on 12/4/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
